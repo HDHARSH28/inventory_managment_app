321975300acb9e32f543dce3eea53b10a9c7278d
HDHARSH28 <hdharsh3204@gmail.com>
2026-02-13 11:36:50 +0530

    Backend on render

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://inventorymanagmentapp-production.up.railway.app/api';
+const API_BASE_URL = 'https://inventory-managment-app-a1cl.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
